9415cd9f0495c5832b552520768ba5db51b44ab7
sameerbabu <sameerbabu.shaik@gmail.com>
2022-10-31 20:08:53 +0530

    Update all_tests.js
    Added a testcase to check if database tables exists and create them if dont exists

--- test/all_tests.js
+++ test/all_tests.js
@@ -5,16 +5,58 @@ process.env.NODE_ENV = "test";
 let chai = require("chai");
 let chaiHttp = require("chai-http");
 let server = require("../server.js");
+
 let should = chai.should();
 let expect = chai.expect;
 var token = "";
 var new_emp_id = "";
 var emp_count = 0;
 
+const Sequelize = require("sequelize");
+const {Umzug, SequelizeStorage} = require("umzug");
+const env = process.env.NODE_ENV || "development";
+const config = require("../app/config/config.json")[env];
+const db = {};
+
+let sequelize;
+sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+
+describe("Checking if DB migration is needed", ()=>{
+  it("it should check if table exists and create tables if dont exists", (done) => {
+    /*****
+   * Because this is standalone DB, and is expected to be deleted and recreated with the server
+   *  Using Sync to create tables if they do not exists, also insert sample data to the tables
+   */
+    sequelize.sync().then(async ()=>{
+      
+      const umzug = new Umzug({
+        migrations: { glob: 'migrations/*.js' },
+        context: sequelize.getQueryInterface(),
+        storage: new SequelizeStorage({ sequelize }),
+        logger: console,
+      });
+      
+      await umzug.up();
+      done();
+
+    })
+  })
+  
+  
+
+});
+
 chai.use(chaiHttp);
 
 //User Login block
 describe("User", () => {
+  
   describe("/POST /api/v1/user/signin", () => {
     it("it should Login using sameer/sameer", (done) => {
       chai
